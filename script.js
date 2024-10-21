37eb34b140a3ffed7c576158dbc05c49b1c6f7d5
D-B-C__SR-DEVARAKONDA <balachandrududevarakonda3@gmail.com>
2024-10-21 18:59:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function run(){
-    let htmlCode = document.getElementById("html-code").value;
-    let cssCode = document.getElementById("css-code").value;
-    let jsCode = document.getElementById("js-code").value;
-    let OutPut = document.getElementById("output");
 
-    OutPut.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
-    OutPut.contentWindow.eval(jsCode);
-}
